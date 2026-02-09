d3be2480b2a7ac628943c40c875edcc337c1b6d6
André <gwandrewgd@gmail.com>
2026-02-09 12:16:03 +0100

    add: added pokemons.json file and template

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from src.PygameApp import PygameApp
-import pokepy
 
 def main():
     game = PygameApp(800, 800)
     game.loop()
-    
-pokemon1 = Pokemon()
 
 if __name__ == "__main__":
     main()
