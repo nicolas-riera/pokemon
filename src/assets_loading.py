fdea1cb67510aa7b9c53726bd18ae502ef2672dd
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-10 10:02:56 +0100

    Moved the json loading into assets_loading.py

--- src/assets_loading.py
+++ src/assets_loading.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,3 +9,6 @@ LOGO_TITLE_RECT = LOGO_TITLE.get_rect(center=(1420, 550))
 LOGO_TITLE_SCALED = pygame.transform.smoothscale(LOGO_TITLE, (LOGO_TITLE.get_size()[0]*0.2, LOGO_TITLE.get_size()[1]*0.2))
 
 CURSOR = pygame.image.load(os.path.join(BASE_DIR, "..", "assets", "img", "cursor.png"))
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "pokemons_type_stats.json"), "r", encoding="UTF-8") as f:
+    POKEMONS_TYPE_STATS = json.load(f)
--- src/game/Combat.py
+++ src/game/Combat.py
@@ -1,6 +1,4 @@
-import json
-import os
-
+from src.assets_loading import POKEMONS_TYPE_STATS
 from src.pokemon.Pokemon import Pokemon
 
 class Combat:
@@ -9,10 +7,9 @@ class Combat:
 
     def calculate_attack_mult(self, attack_type:str, enemy:object):
         mult = 1
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data", "pokemons_type_stats.json"), "r", encoding="UTF-8") as f:
-            types = json.load(f)
+        
         for def_type in enemy.get_types():
-            mult *= types[attack_type][def_type]
+            mult *= POKEMONS_TYPE_STATS[attack_type][def_type]
         return mult
             
 
