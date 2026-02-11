6899904d08919b193146b3b615bd26bf9f74da66
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-11 12:06:53 +0100

    fix: changed add pokemon to enemy pokemon for clarification

--- src/Menu.py
+++ src/Menu.py
@@ -4,9 +4,9 @@ from src.assets_loading import LOGO_TITLE_SCALED, LOGO_TITLE_RECT, CURSOR
 
 class Menu:
     def __init__(self):
-        self.__play_button = pygame.Rect((200, 405, 400, 75))
-        self.__add_pokemon_button = pygame.Rect((200, 485, 400, 70))
-        self.__pokedex_button = pygame.Rect((200, 560, 400, 75))
+        self.__play_button = pygame.Rect((180, 405, 440, 75))
+        self.__enemy_pokemon_button = pygame.Rect((180, 485, 440, 70))
+        self.__pokedex_button = pygame.Rect((180, 560, 440, 75))
 
     def menu_rendering(self, screen, font):
 
@@ -14,23 +14,23 @@ class Menu:
 
         play_button_text = font.render("Play", True, (0, 0, 0))
         screen.blit(play_button_text, (355, 430))
-        add_pokemon_button_text = font.render("Add Pokemon", True, (0, 0, 0))
-        screen.blit(add_pokemon_button_text, (260, 500))
+        enemy_pokemon_button_text = font.render("Enemy Pokemon", True, (0, 0, 0))
+        screen.blit(enemy_pokemon_button_text, (235, 500))
         pokedex_button_text = font.render("Pokedex", True, (0, 0, 0))
         screen.blit(pokedex_button_text, (307, 570))
 
-        pygame.draw.line(screen, (0, 0, 0), (200, 400), (600, 400), width=5)
-        pygame.draw.line(screen, (0, 0, 0), (200, 640), (600, 640), width=5)
+        pygame.draw.line(screen, (0, 0, 0), (180, 400), (620, 400), width=5)
+        pygame.draw.line(screen, (0, 0, 0), (180, 640), (620, 640), width=5)
 
-        pygame.draw.line(screen, (0, 0, 0), (200, 400), (200, 640), width=5)
-        pygame.draw.line(screen, (0, 0, 0), (600, 400), (600, 640), width=5)
+        pygame.draw.line(screen, (0, 0, 0), (180, 400), (180, 640), width=5)
+        pygame.draw.line(screen, (0, 0, 0), (620, 400), (620, 640), width=5)
 
         if self.__play_button.collidepoint(pygame.mouse.get_pos()):
-            screen.blit(CURSOR, (210, 430)) 
-        elif self.__add_pokemon_button.collidepoint(pygame.mouse.get_pos()):
-            screen.blit(CURSOR, (210, 500)) 
+            screen.blit(CURSOR, (190, 430)) 
+        elif self.__enemy_pokemon_button.collidepoint(pygame.mouse.get_pos()):
+            screen.blit(CURSOR, (190, 500)) 
         elif self.__pokedex_button.collidepoint(pygame.mouse.get_pos()):
-            screen.blit(CURSOR, (210, 570))
+            screen.blit(CURSOR, (190, 570))
 
 
     def menu_logic(self, escpressed, mouseclicked, state):
@@ -38,13 +38,13 @@ class Menu:
         if escpressed: 
             pygame.quit()
             raise SystemExit
-        if self.__play_button.collidepoint(pygame.mouse.get_pos()) or self.__add_pokemon_button.collidepoint(pygame.mouse.get_pos()) or self.__pokedex_button.collidepoint(pygame.mouse.get_pos()):
+        if self.__play_button.collidepoint(pygame.mouse.get_pos()) or self.__enemy_pokemon_button.collidepoint(pygame.mouse.get_pos()) or self.__pokedex_button.collidepoint(pygame.mouse.get_pos()):
             if mouseclicked:
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
                 if self.__play_button.collidepoint(pygame.mouse.get_pos()):
                     state = "game"
-                elif self.__add_pokemon_button.collidepoint(pygame.mouse.get_pos()):
-                    state = "add_pokemon"
+                elif self.__enemy_pokemon_button.collidepoint(pygame.mouse.get_pos()):
+                    state = "enemy_pokemon"
                 else:
                     state = "pokedex"
             else:
