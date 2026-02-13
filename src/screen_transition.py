7ca14b79a4e46dad16425bb9aa1ce848b2b1233f
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-13 13:01:26 +0100

    add: added battle music and improved music logic

--- src/screen_transition.py
+++ src/screen_transition.py
@@ -1,7 +1,15 @@
 from pygame import display
 from time import sleep
 
+import pygame
+
 def screen_transition(screen):
+
+    pygame.mixer.music.pause()
+    pygame.mixer.music.unload()
+
     screen.fill("white")
     display.flip()
     sleep(0.5)
+
+    
