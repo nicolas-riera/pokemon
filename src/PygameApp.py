bc904003a4ee440c14b7fc170d4af6ce36249b44
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-02-17 11:37:39 +0100

    feat(pokedex): implement pokemon deletion via right-click
       - Updated PygameApp to distinguish between and track left and right mouse clicks.
       - Enhanced pokedex_logic to accept both click types.
       - Implemented functionality to delete a Pokémon from the Pokedex via right-click, including JSON data persistence and object list refreshing.

--- src/PygameApp.py
+++ src/PygameApp.py
@@ -32,7 +32,8 @@ class PygameApp:
 
     def events(self):
         self.escpressed = False
-        self.mouseclicked = False
+        self.mouseclicked_left = False
+        self.mouseclicked_right = False
 
         for event in pygame.event.get():
 
@@ -40,8 +41,10 @@ class PygameApp:
                 self.running = False
             
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                self.mouseclicked = True
-
+                self.mouseclicked_left = True
+            
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                self.mouseclicked_right = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.escpressed = True 
@@ -77,11 +80,11 @@ class PygameApp:
         prev_state = self.state
 
         if self.state == "menu":
-            self.state = self.menu.menu_logic(self.escpressed, self.mouseclicked, self.state)
+            self.state = self.menu.menu_logic(self.escpressed, self.mouseclicked_left, self.state)
         elif self.state in ["game", "choose_attack", "pre_attack"]:
             self.state = self.combat.logic(self.pokedex.pokedex_objects[0], self.escpressed)
         elif self.state == "pokedex":
-            self.state = self.pokedex.pokedex_logic(self.escpressed, self.state, self.mouseclicked)
+            self.state = self.pokedex.pokedex_logic(self.escpressed, self.state, self.mouseclicked_left, self.mouseclicked_right)
 
         if prev_state != self.state :
             screen_transition(self.screen, self.clock, self.state)
